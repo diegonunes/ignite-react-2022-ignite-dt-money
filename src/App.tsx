28b689b4e3c9257e5928f161484744fc78ae74f2
Diego Nunes <diegonunesdev@gmail.com>
2022-08-22 22:13:55 -0300

    finished chapter II

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { TransactionsProvider } from './contexts/TransactionsContext';
 import { Transactions } from './pages/Transactions';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
@@ -9,7 +9,9 @@ export const App = () => {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   );
 };
